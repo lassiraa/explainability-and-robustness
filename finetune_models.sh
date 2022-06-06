dbaa848ef000a3501fc8106d40b7c996554b8722
Lassi Raatikainen <raatikainenlassi@gmail.com>
2022-06-06 08:38:39 +0300

    add shell script for easy fine-tuning

---
+++ finetune_models.sh
@@ -0,0 +1,45 @@
+# Training parameters for replication
+model_name="vgg16"
+lr="1e-3"
+gamma=0.9
+batch_size=64
+epochs=50
+
+# Desired paths and num workers
+images_dir="/media/lassi/Data/datasets/coco/images/"
+ann_dir="/media/lassi/Data/datasets/coco/annotations/"
+checkpoint_dir="/media/lassi/Data/checkpoints/"
+num_workers=16
+
+# Parameters for wandb.init (experiment tracking)
+wandb_entity="lassiraa"
+wandb_project="coco-finetune"
+
+
+python fine_tune.py --images_dir ${images_dir} \
+    --ann_dir ${ann_dir} \
+    --checkpoint_dir ${checkpoint_dir} \
+    --model_name ${model_name} \
+    --lr ${lr} \
+    --gamma ${gamma} \
+    --epochs ${epochs} \
+    --batch_size ${batch_size} \
+    --num_workers ${num_workers} \
+    --wandb_entity ${wandb_entity} \
+    --wandb_project ${wandb_project}
+
+
+# Next model to train
+model_name="vit_b_32"
+
+python fine_tune.py --images_dir ${images_dir} \
+    --ann_dir ${ann_dir} \
+    --checkpoint_dir ${checkpoint_dir} \
+    --model_name ${model_name} \
+    --lr ${lr} \
+    --gamma ${gamma} \
+    --epochs ${epochs} \
+    --batch_size ${batch_size} \
+    --num_workers ${num_workers} \
+    --wandb_entity ${wandb_entity} \
+    --wandb_project ${wandb_project}

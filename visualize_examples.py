103c0eba3853061420182e76883fe0665a222c45
Lassi Raatikainen <raatikainenlassi@gmail.com>
2022-06-22 09:39:17 +0300

    change script to better reproducible and to save images to folder

--- visualize_examples.py
+++ visualize_examples.py
@@ -1,31 +1,45 @@
-from random import randint 
+import random
 
 import torchvision.transforms as transforms
+import torch
 
 from utils import CocoDistortion
+
+
+torch.random.manual_seed(50)
+random.seed(50)
 
 
 if __name__ ==  '__main__':
     test_transform = transforms.Compose([
         transforms.Resize(224),
+        # transforms.RandomRotation(10),
         transforms.CenterCrop(224)
     ])
     path2data = "/media/lassi/Data/datasets/coco/images/val2017/"
     path2json = "/media/lassi/Data/datasets/coco/annotations/instances_val2017.json"
     path2idjson = "data/image_to_annotation.json"
+
+    distortion_method = 'perpendicular'
+    distort_background = 'smooth_transition'
+
     coco_dset = CocoDistortion(
         root=path2data,
         annFile=path2json,
         imToAnnFile=path2idjson,
         transform=test_transform,
         target_transform=None,
-        distort_background=None
+        distort_background=distort_background,
+        distortion_method=distortion_method,
+        debug_mode=True
     )
 
     num_ids = len(coco_dset.ids)
+    i = 0
     while True:
         image, image_distorted, target = \
-            coco_dset.__getitem__(randint(0, num_ids))
-        image.show()
-        image_distorted.show()
-        input("Press Enter to show next image...")
+            coco_dset.__getitem__(random.randint(0, num_ids))
+        image.save(f'./examples/{i}.png')
+        image_distorted.save(f'./examples/{i}_{distortion_method}_{distort_background}.png')
+        i += 1
+        input("Press Enter to get next image...")

0b8939dc98da9a7e91d95b38924e883460ac1382
Lassi Raatikainen <raatikainenlassi@gmail.com>
2022-07-08 09:51:59 +0300

    add guided backpropagation and fullgrad to functional saliency methods

--- explanation_stability.py
+++ explanation_stability.py
@@ -7,6 +7,7 @@ import torch
 import torchvision.transforms as transforms
 import numpy as np
 from scipy import stats
+from tqdm import tqdm
 from pytorch_grad_cam import GradCAM, \
     ScoreCAM, \
     GradCAMPlusPlus, \
@@ -15,7 +16,8 @@ from pytorch_grad_cam import GradCAM, \
     EigenCAM, \
     EigenGradCAM, \
     LayerCAM, \
-    FullGrad
+    FullGrad, \
+    GuidedBackpropReLUModel
 from pytorch_grad_cam.ablation_layer import AblationLayerVit
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 
@@ -28,18 +30,28 @@ def calculate_mean_correlation(
     saliency_method: Callable,
     class_idx: int,
     image_transform: transforms.Compose,
-    image_normalize: transforms.Normalize
+    image_normalize: transforms.Normalize,
+    is_backprop: bool
 ) -> tuple[np.ndarray]:
     correlations = []
     prev_saliency = None
 
-    for frame in video.iter_frames():
+    for i, frame in enumerate(video.iter_frames()):
+        
+        #  Only process first 10 frames and last 10 frames
+        if i > 10 and i < 139:
+            prev_saliency = None
+            continue
+
         #  Preprocess frame for model
         frame = image_transform(frame / 255)
         input = image_normalize(frame).to(device=device, dtype=torch.float32).unsqueeze(0)
 
         #  Process saliency map
-        saliency_map = saliency_method(input, [ClassifierOutputTarget(class_idx)])[0, :]
+        if is_backprop:
+            saliency_map = saliency_method(input, target_category=class_idx)
+        else:
+            saliency_map = saliency_method(input, [ClassifierOutputTarget(class_idx)])[0, :]
 
         #  Calculate Spearman correlation of saliency map from previous frame to current frame
         if prev_saliency is not None:
@@ -56,12 +68,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Measure stability of explanation method')
     parser.add_argument('--in_path', type=str, required=True,
                         help='path folder containing videos')
-    parser.add_argument('--out_path', type=str, required=True,
-                        help='path to output json')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='batch size for cam methods')
-    parser.add_argument('--num_workers', type=int, default=16,
-                        help='workers for dataloader')
     parser.add_argument('--model_name', type=str, default='resnet50',
                         help='name of model used for inference',
                         choices=[
@@ -73,7 +81,8 @@ if __name__ == '__main__':
                         choices=['gradcam', 'gradcam++',
                                  'scorecam', 'xgradcam',
                                  'ablationcam', 'eigencam',
-                                 'eigengradcam', 'layercam', 'fullgrad'],
+                                 'eigengradcam', 'layercam', 'fullgrad',
+                                 'guidedbackprop'],
                         help='Can be gradcam/gradcam++/scorecam/xgradcam'
                              '/ablationcam/eigencam/eigengradcam/layercam')
     args = parser.parse_args()
@@ -107,10 +116,17 @@ if __name__ == '__main__':
          "ablationcam": AblationCAM,
          "xgradcam": XGradCAM,
          "eigencam": EigenCAM,
-         "fullgrad": FullGrad}
+         "fullgrad": FullGrad,
+         "layercam": LayerCAM,
+         "guidedbackprop": GuidedBackpropReLUModel}
 
     method = methods[args.method]
-    if args.method == 'ablationcam' and is_vit:
+    is_backprop = False
+    if args.method == 'guidedbackprop':
+        saliency_method = method(model=model,
+                                 use_cuda=torch.cuda.is_available())
+        is_backprop = True
+    elif args.method == 'ablationcam' and is_vit:
         saliency_method = method(model=model,
                                  target_layers=target_layers,
                                  reshape_transform=reshape_transform,
@@ -123,7 +139,7 @@ if __name__ == '__main__':
                                  use_cuda=torch.cuda.is_available())
     
     corrs = []
-    for fname in os.listdir(args.in_path):
+    for fname in tqdm(os.listdir(args.in_path)):
         video = mpy.VideoFileClip(args.in_path+fname)
         #  Use highest probability class from first frame for CAM
         start_frame = image_transform(video.get_frame(0)).to(device).unsqueeze(0)
@@ -136,7 +152,8 @@ if __name__ == '__main__':
             saliency_method=saliency_method,
             class_idx=class_idx,
             image_transform=image_transform,
-            image_normalize=image_normalize
+            image_normalize=image_normalize,
+            is_backprop=is_backprop
         )
     
         corrs.append(corr)
---
+++ measure_explanation_stability.sh
@@ -0,0 +1,14 @@
+# Directory of videos containing 3D effect (150 frames each)
+video_dir="/media/lassi/Data/datasets/coco/3d-effect-videos/val2017/"
+methods="gradcam gradcam++ xgradcam layercam guidedbackprop"
+models="resnet50 vit_b_32 vgg16"
+
+
+for model in $models; do
+    for method in $methods; do
+        python explanation_stability.py \
+            --method ${method} \
+            --model ${model} \
+            --in_path ${video_dir}
+    done
+done

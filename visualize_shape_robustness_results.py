8222e0632faf6deaba92511fe4d2601cb9afc186
Lassi Raatikainen <raatikainenlassi@gmail.com>
2022-07-11 14:43:54 +0300

    change filename

--- visualize_shape_robustness_results.py
+++ visualize_shape_robustness_results.py
@@ -11,15 +11,15 @@ files = os.listdir(path)
 results = []
 
 for file in files:
-    if 'results' not in file:
+    if 'shape_robustness' not in file:
         continue
-    model_name = file.replace('_results.json', '')
+    model_name = file.replace('_shape_robustness.json', '')
     with open(f'{path}{file}', 'r') as f:
         model_res = json.load(f)
         results += model_res
 
 results_df = pd.DataFrame.from_records(results)
-results_df = results_df[results_df['distortion_method'] == 'singular_spike']
+results_df = results_df[results_df['distort_background'].isnull()]
 sns.set_theme(style="ticks", color_codes=True)
-ax = sns.scatterplot(x='distort_background', y='distort_ratio', hue='model_name', style='model_name', data=results_df)
+ax = sns.scatterplot(x='distortion_method', y='distort_ratio', hue='model_name', style='model_name', data=results_df)
 plt.show()
